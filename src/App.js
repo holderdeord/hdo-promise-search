748ce0ebe8c9f1733e86507d9b8b54fdf34df397
Jari Bakken <jari.bakken@gmail.com>
2020-02-04 13:05:22 +0100

    shutdown note

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
-import Header from './Header';
-import Search from './Search';
-import Services from './Services';
-import Footer from './Footer';
+import Header from "./Header";
+import Search from "./Search";
+import Services from "./Services";
+import Footer from "./Footer";
 
-export default () =>
+export default () => (
     <div>
         <Header />
+        <div className="container">
+            <div className="alert alert-info" style={{ display: "none" }}>
+                <strong>Holder de ord</strong> legges ned i løpet av 2020. Denne
+                tjenesten vil ikke bli oppdatert.
+            </div>
+        </div>
+
         <Search />
         <Services />
         <Footer />
-    </div>;
+    </div>
+);
